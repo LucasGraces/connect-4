26c7af8752ada8c747a89d1e698db77386793169
Dam-2211 <mirandadavid980@gmail.com>
2022-10-20 10:21:58 -0300

    modificacion de prueba

--- Juego/datos.cpp
+++ Juego/datos.cpp
@@ -63,12 +63,62 @@ void Prueba::tablero(){
 
 void Prueba::tablero_juego(){
   
-   
+   int col=0;
+
+   for(int l = 0; l < 42 ;l++){
+      cout<<"| "<<tablero[l];
+      col++;
+  
+      if(col==7) {
+         col=0;
+         cout<<" |"<<endl;
+      }
+   }
 };
 bool Prueba::ver_ganador(){
-   
+   bool resultado = false;
+
+   for(int i = 0; i < 294; i = i + 4){
+      if(tablero[lineas[i]] == tablero[lineas[i + 1]]
+         && tablero[lineas[i]] == tablero[lineas[i + 2]]
+         && tablero[lineas[i]] == tablero[lineas[i + 3]]
+         && tablero[lineas[i]] != '\0'){
+            resultado = true;
+      }   
+   }
+   return resultado;
 };
 
 void Prueba::agregarficha(bool jugadores){
-   
+   int o;
+   int l;
+   ficha--;
+   bool flag = false;
+   bool fla = false;
+
+   if(jugadores == true){
+      for( o = ficha; o < 42; o = o + 7){
+         if(tablero[o] != '\0'){
+            tablero[o-7] = 'X'; 
+            flag=true;
+            break;
+         }
+      }
+      if(flag==false){
+         tablero[35+ficha]='X';
+      }
+
+   } 
+  if(jugadores == false){
+     for( l = ficha; l < 42; l = l + 7){
+         if(tablero[l] != '\0'){
+            tablero[l-7] = 'O'; 
+            fla=true;
+            break;
+         }
+      }
+      if(fla == false){
+         tablero[35+ficha] = 'O';
+      }
+   } 
 }; 
